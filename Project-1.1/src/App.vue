928201925ea412661bb8c1298b4d18c88e28e550
Matt Jones <Matt.Jones@1ststop.co.uk>
2018-08-24 15:28:32 +0100

    Vue filter added

---
+++ Project-1.1/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1> {{ title }} </h1>
+
+    <Navbar />
+    <Blogs />
+
+  </div>
+</template>
+
+<script>
+
+import Navbar from './Navbar'
+import Blogs from './Blogs'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Blogs
+  },
+  data () {
+    return {
+      title: 'Vue Project-1',
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
